c61840b66623bc11218e1dfc577b75564ae41161
Jay Wen <jaywen89@gmail.com>
2019-11-03 22:22:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,10 +9,8 @@ def introduction (name)
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
-def introduction_with_language_optional 
-
-
-
+def introduction_with_language_optional (name, )
+  puts "Hi, my name is "
 
 
 
